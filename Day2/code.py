d5a6d8c7a1a1a7bbfd02cfa39e12c954c367d300
zenit-0 <106954296+zenit-0@users.noreply.github.com>
2022-12-03 21:28:46 +0100

    Update code.py

--- Day2/code.py
+++ Day2/code.py
@@ -9,18 +9,28 @@ import numpy as np
 
 #initialize
 input = np.loadtxt("input.txt", dtype='str')
-score = 0
+score_naive = 0
+score_real = 0
 i = 0
+real_input = input[:,1]
 
-#dict to skip if statements
 value_scores = {'X':1, 'Y':2, 'Z':3}
 game_scores = {'AX':3, 'BY':3, 'CZ':3, 'AY':6, 'BZ':6, 'CX':6, 'AZ':0, 'BX':0, 'CY':0}
 
-#scan games array to calculate score
 for i in np.arange(0,input[:,0].size,1):
     game = input[i,0]+input[i,1]
-    score = score + value_scores[game[1]]+game_scores[game]
+    score_naive = score_naive + value_scores[game[1]]+game_scores[game]
+    i = i+1
+    
+print(score_naive)
+
+i = 0
+real_scores = {'AX':'Z', 'AY':'X', 'AZ':'Y', 'BX':'X', 'BY':'Y', 'BZ':'Z', 'CX':'Y', 'CY':'Z', 'CZ':'X'}
+
+for i in np.arange(0,input[:,0].size,1):
+    game = input[i,0]+input[i,1]
+    real_input = real_scores[game]
+    score_real = score_real + game_scores[game[0]+real_input] + value_scores[real_input]
     i = i+1
 
-#print score
-print(score)
+print(score_real)
