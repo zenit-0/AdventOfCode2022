5fbce1de8d9e9d7da7bfd1ff8ec4a2b62a469a5f
zenit-0 <106954296+zenit-0@users.noreply.github.com>
2022-12-03 21:10:18 +0100

    Update code.py

--- Day2/code.py
+++ Day2/code.py
@@ -12,12 +12,15 @@ input = np.loadtxt("input.txt", dtype='str')
 score = 0
 i = 0
 
+#dict to skip if statements
 value_scores = {'X':1, 'Y':2, 'Z':3}
 game_scores = {'AX':3, 'BY':3, 'CZ':3, 'AY':6, 'BZ':6, 'CX':6, 'AZ':0, 'BX':0, 'CY':0}
 
+#scan games array to calculate score
 for i in np.arange(0,input[:,0].size,1):
     game = input[i,0]+input[i,1]
     score = score + value_scores[game[1]]+game_scores[game]
     i = i+1
-    
+
+#print score
 print(score)
