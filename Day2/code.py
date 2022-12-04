2f3c7d3b9fae1a4a0a4f2fefe3d7efbdb18f372e
zenit-0 <106954296+zenit-0@users.noreply.github.com>
2022-12-04 12:56:52 +0100

    Update code.py
    Removed "input" as a variable name

--- Day2/code.py
+++ Day2/code.py
@@ -8,24 +8,24 @@ Created on Sat Dec  3 19:55:09 2022
 import numpy as np
 
 #initialize
-input = np.loadtxt("input.txt", dtype='str')
+data = np.loadtxt("data.txt", dtype='str')
 score_naive = 0
 score_real = 0
-real_input = input[:,1]
+real_input = data[:,1]
 
 value_scores = {'X':1, 'Y':2, 'Z':3}
 game_scores = {'AX':3, 'BY':3, 'CZ':3, 'AY':6, 'BZ':6, 'CX':6, 'AZ':0, 'BX':0, 'CY':0}
 
-for i in np.arange(0,input[:,0].size,1):
-    game = input[i,0]+input[i,1]
+for i in np.arange(0,data[:,0].size,1):
+    game = data[i,0]+data[i,1]
     score_naive = score_naive + value_scores[game[1]]+game_scores[game]
     
 print(score_naive)
 
 real_scores = {'AX':'Z', 'AY':'X', 'AZ':'Y', 'BX':'X', 'BY':'Y', 'BZ':'Z', 'CX':'Y', 'CY':'Z', 'CZ':'X'}
 
-for i in np.arange(0,input[:,0].size,1):
-    game = input[i,0]+input[i,1]
+for i in np.arange(0,data[:,0].size,1):
+    game = data[i,0]+data[i,1]
     real_input = real_scores[game]
     score_real = score_real + game_scores[game[0]+real_input] + value_scores[real_input]
 
